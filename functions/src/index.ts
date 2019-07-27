536961f58189939b1c1e7d9d2c4fae07d6c4bd0a
Michael Maier <maier1michael@gmail.com>
2019-07-27 16:27:36 +0200

    get forecast data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,58 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+function getTemperatureGroupData($temperatureGroup: Cheerio) {
+  return {
+    max: $temperatureGroup.find('.tmax').text(),
+    min: $temperatureGroup.find('.tmin').text(),
+    snow: $temperatureGroup.find('.nschnee').text().trim(),
+  };
+}
+
+async function getForecast(url: string) {
+  const {data} = await axios.get(url);
+  const $ = cheerio.load(data);
+  const $container = $('.forecast9d-container');
+  return $container
+    .find('.day')
+    .map((_, dayElement) => {
+      const $day = $(dayElement);
+      const date = new Date();
+      const title = $day.attr('title');
+      const dateMatch = title.match(/(\d+)\.(\d+)\.(\d+)/);
+      if (!dateMatch) {
+        throw Error(`Could not parse date in title: "${title}"`);
+      }
+      const [, day, month, year] = dateMatch;
+      date.setUTCFullYear(parseInt(year), parseInt(month) - 1, parseInt(day));
+      const timeStr = $container.find('.time').text();
+      const timeMatch = timeStr.match(/(\d+):(\d+)/);
+      if (!timeMatch) {
+        throw Error(`Could not parse time: "${timeStr}"`);
+      }
+      const [, hours, minutes] = timeMatch;
+      date.setUTCHours(parseInt(hours), parseInt(minutes));
+      const $mountain = $day.find('.group').eq(0);
+      const $valley = $day.find('.group').eq(1);
+      const mountain = getTemperatureGroupData($mountain);
+      const valley = getTemperatureGroupData($valley);
+      return {
+        date,
+        mountain,
+        valley,
+        probabilityOfPrecipitation: $day.find('.rrp').text().trim(),
+        rainfall: $day.find('.rrp').text().trim(),
+        sun: $day.find('.sonne').text().trim(),
+        snowLine: $day.find('.sgrenze').text().trim(),
+        thunderstorm: $day.find('.wgew').text().trim(),
+        wind: $day.find('.ff').text().trim(),
+      };
+    })
+    .get();
+}
+
+export const forecast = functions.https.onRequest(async (request, response) => {
+  const result = await getForecast('https://www.bergfex.at/hintertux/wetter/prognose/');
+  response.send(JSON.stringify(result, null, 4));
+});
